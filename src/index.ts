44b15431603c4c8c1533a99518e42367aa07fced
Masaki Hara <ackie.h.gmai@gmail.com>
2021-04-23 17:19:38 +0900

    Command line

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,33 @@
 import fs from "fs";
 import process from "process";
+import yargs from "yargs";
 import { convertLockfile } from "./convert";
 
+const { f: inputPath, o: outputPath } = yargs
+  .scriptName('yarn-lock-v1')
+  .option('f', {
+    alias: 'file',
+    describe: 'input yarn.lock file (v2)',
+    default: 'yarn.lock',
+    type: 'string',
+  })
+  .option('o', {
+    alias: 'output',
+    describe: 'output yarn.lock file (v1)',
+    default: 'yarn.lock',
+    type: 'string',
+  })
+  .strict()
+  .help()
+  .argv;
+
 (async () => {
-  const lockV2 = await fs.promises.readFile('yarn.lock', 'utf-8');
+  const lockV2 = await fs.promises.readFile(inputPath, 'utf-8');
   const lockV1 = await convertLockfile(lockV2);
-  await fs.promises.writeFile('yarn.lock.v1', lockV1);
+  if (inputPath === outputPath) {
+    await fs.promises.copyFile(inputPath, `${inputPath}.bak`);
+  }
+  await fs.promises.writeFile(outputPath, lockV1);
 })().catch((e) => {
   console.error(e);
   process.exit(1);
