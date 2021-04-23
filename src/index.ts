436cca3aa554a0078eca6b772e17c630329a17f0
Masaki Hara <ackie.h.gmai@gmail.com>
2021-04-23 16:27:53 +0900

    Make index.ts async

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import fs from "fs";
+import process from "process";
 import { convertLockfile } from "./convert";
 
-const lockV2 = fs.readFileSync('yarn.lock', 'utf-8');
-const lockV1 = convertLockfile(lockV2);
-fs.writeFileSync('yarn.lock.v1', lockV1);
+(async () => {
+  const lockV2 = await fs.promises.readFile('yarn.lock', 'utf-8');
+  const lockV1 = convertLockfile(lockV2);
+  await fs.promises.writeFile('yarn.lock.v1', lockV1);
+})().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
