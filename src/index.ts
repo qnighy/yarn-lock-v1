4a1372e559e7d5f6dde67e1ef0e132c8982ea585
Masaki Hara <ackie.h.gmai@gmail.com>
2021-04-23 16:54:05 +0900

    Query yarn registry

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { convertLockfile } from "./convert";
 
 (async () => {
   const lockV2 = await fs.promises.readFile('yarn.lock', 'utf-8');
-  const lockV1 = convertLockfile(lockV2);
+  const lockV1 = await convertLockfile(lockV2);
   await fs.promises.writeFile('yarn.lock.v1', lockV1);
 })().catch((e) => {
   console.error(e);
