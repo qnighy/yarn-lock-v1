c6fcb4760359b6165887d1d1e144e6be4ddea82b
Masaki Hara <ackie.h.gmai@gmail.com>
2021-05-02 07:16:38 +0900

    Add tests for token

--- src/convert.test.ts
+++ src/convert.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, it, jest } from "@jest/globals";
 import fs from "fs";
 import path from "path";
 import { setupRecorder } from "nock-record";
-import nodeFetch, { Response } from "node-fetch";
+import nodeFetch, { Headers, Response } from "node-fetch";
 
 import { convertLockfile, Fetcher } from "./convert";
 
@@ -73,5 +73,39 @@ describe("convertLockfile", () => {
         "Error reading is-regex/1.1.2: Got 503 from https://registry.yarnpkg.com/is-regex/1.1.2",
       ],
     ]);
+  });
+
+  it("sends Bearer token when requested", async () => {
+    const token = "8dd79e01-d0a4-446b-b261-d6d6408227d2";
+    const fetch: Fetcher = async (url, init) => {
+      const { headers = new Headers() } = init ?? {};
+      if (/is-regex\/1\.1\.2/.test(url)) {
+        if (headers.get("Authorization") === `Bearer ${token}`) {
+          return nodeFetch(url);
+        } else {
+          return new Response("", { status: headers.get("Authorization") ? 403 : 401, url });
+        }
+      } else {
+        return nodeFetch(url);
+      }
+    };
+    const interaction = {
+      warn: jest.fn<string, []>(),
+    };
+    const { completeRecording, assertScopesFinished } = await record(
+      "yarn-lock1"
+    );
+
+    const lockV2 = fs.readFileSync(
+      path.resolve(__dirname, "./__fixtures__/yarn-lock1.txt"),
+      "utf-8"
+    );
+    const lockV1 = await convertLockfile(lockV2, { fetch, interaction, yarnrc: { npmAuthToken: token } });
+    expect(lockV1).toMatchSnapshot("yarn-lock1-offline");
+
+    completeRecording();
+    assertScopesFinished();
+
+    expect(interaction.warn.mock.calls).toEqual([]);
   });
 });
